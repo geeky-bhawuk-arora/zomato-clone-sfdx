6aea8fa1a64ebc9b5f86485321d89b2b3f2187aa
Bhawuk Arora <bhawukarora042@gmail.com>
2025-07-08 01:22:26 +0530

    restro view functionality done

--- force-app/main/default/lwc/restaurantList/restaurantList.js
+++ force-app/main/default/lwc/restaurantList/restaurantList.js
@@ -3,8 +3,8 @@ import getRestaurants from '@salesforce/apex/RestaurantController.getRestaurants
 
 export default class RestaurantList extends LightningElement {
     @api cuisine;
-    @api rating;
+    @api rating; 
 
-    @wire(getRestaurants, { cuisine: '$cuisine', minRating: '$rating' })
+    @wire(getRestaurants, { cuisine: '$cuisine', minRating: '$rating' }) // props
     restaurants;
 }
